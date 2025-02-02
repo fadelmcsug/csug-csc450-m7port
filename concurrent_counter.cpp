70da8053b6690645fbfaaf81aec350ec69aa92c3
fadelmcsug <fadel.makhzoum@csuglobal.edu>
2025-02-02 10:08:31 -0600

    Add files via upload

--- concurrent_counter.cpp
+++ concurrent_counter.cpp
@@ -14,15 +14,16 @@
 #include <mutex>
 
 std::mutex mtx;
+int counter;
 
-void countTo(int start, int end, std::string counterName) {
+void countTo(int* start, int end, std::string counterName) {
     while (true) {
         std::lock_guard<std::mutex> lock(mtx);
-        std::cout << counterName << ": " << start << std::endl;
-        if (start < end) {
-            start++;
-        } else if (start > end) {
-            start--;
+        std::cout << counterName << ": " << *start << std::endl;
+        if (*start < end) {
+            (*start)++;
+        } else if (*start > end) {
+        	(*start)--;
         } else {
             break;  // Exit the loop when start equals end
         }
@@ -30,10 +31,10 @@ void countTo(int start, int end, std::string counterName) {
 }
 
 int main() {
-    std::thread t1(countTo, 0, 20, "thread 1 counter");
+    std::thread t1(countTo, &counter, 20, "thread 1 counter");
     t1.join();
 
-    std::thread t2(countTo, 20, 0, "thread 2 counter");
+    std::thread t2(countTo, &counter, 0, "thread 2 counter");
     t2.join();
 
     return 0;
